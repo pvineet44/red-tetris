1218ce67e21219459b5dc3e6323842a5bc268f72
Vineet Parekh <pvineet44@gmail.com>
2021-04-08 09:59:41 +0530

    client

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -9,6 +9,14 @@ const PORT: string | number = process.env.PORT || 4000
 
 app.use(cors())
 app.use(routes)
+if(process.env.NODE_ENV=="production"){
+        app.use(express.static('../client/build'))
+        const path = require('path')
+        app.get("*",(req,res)=>{
+            res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+        })
+    }
+    
         console.log(`Server running on http://localhost:${PORT}`)
 
 // const uri: string = 'mongodb+srv://admin:admin@cluster0.s5t7m.mongodb.net/red-tetris'
