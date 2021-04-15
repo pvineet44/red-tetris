b6471f295cf83043cf317716ce0d9f620685f953
Vineet Parekh <pvineet44@gmail.com>
2021-04-15 14:27:08 +0530

    app.js

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,10 +17,10 @@ const PORT: string | number = process.env.PORT || 5000
 app.use(cors())
 // app.use(routes)
 if(process.env.NODE_ENV=="production"){
-        app.use(express.static('../public/'))
+        app.use(express.static('../../public/'))
         const path = require('path')
         app.get("*",(req,res)=>{
-            res.sendFile(path.resolve(__dirname,'../public','index.html'))
+            res.sendFile(path.resolve(__dirname,'../../public','index.html'))
         })
     }
     
