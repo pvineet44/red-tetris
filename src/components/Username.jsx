1ce5b75fcd3bc74bd4942b69111860827e53c232
Vineet Parekh <pvineet44@gmail.com>
2021-04-16 18:28:06 +0530

    eslint

--- src/components/Username.jsx
+++ src/components/Username.jsx
@@ -1,19 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Username = (socket) => {
+  const [input, setInput] = useState("");
 
-    const [input, setInput] = useState('');
+  const handleSubmit = () => {
+    console.log("input: ", input, socket);
+  };
 
-    return (
-        <div>
-            <h2>Enter your name: </h2>
-            <input
-                type="text"
-                name="inputfield"
-                onChange={(e) => setInput(e.target.value)}
-            />
-            <button type="button" onClick={() => console.log('nas: ', input)} >Hello</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Enter your name: </h2>
+      <input
+        type="text"
+        name="inputfield"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="button" onClick={() => handleSubmit}>
+        Hello
+      </button>
+    </div>
+  );
+};
 export default Username;
