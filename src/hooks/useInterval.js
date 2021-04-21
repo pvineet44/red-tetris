ffcb055c7fa6c4354ce1584b41d1c6b5c58cc4a8
Mohit <mohitashar1995@gmail.com>
2021-04-21 14:52:47 +0530

    useInterval test

--- src/hooks/useInterval.js
+++ src/hooks/useInterval.js
@@ -4,14 +4,11 @@ export function useInterval(callback, delay) {
   const savedCallback = useRef();
   // Remember the latest callback.
   useEffect(() => {
-    console.log('A TEST----------------------------------')
     savedCallback.current = callback;
   }, [callback]);
 
   // Set up the interval.
   useEffect(() => {
-    console.log('B TEST----------------------------------')
-
     function tick() {
       savedCallback.current();
     }
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -155,3 +155,55 @@ it("useInterval stage", () => {
   // useInterval(callbackMock, 1000);
 
 })
+
+
+describe("useInterval stage", () => {
+  // const wrapper = shallow(<Welcome />);
+  let callback = jest.fn();
+
+  beforeAll(() => {
+    // we're using fake timers because we don't want to
+    // wait a full second for this test to run.
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    callback.mockRestore();
+    jest.clearAllTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  test('should init hook with delay', () => {
+    const { result } = renderHook(() => useInterval(callback, 5000));
+
+    expect(result.current).toBeUndefined();
+    expect(setInterval).toHaveBeenCalledTimes(1);
+    expect(setInterval).toHaveBeenCalledWith(expect.any(Function), 5000);
+  });
+
+  test('should repeatedly calls provided callback with a fixed time delay between each call', () => {
+    const { result } = renderHook(() => useInterval(callback, 200));
+    expect(callback).not.toHaveBeenCalled();
+
+    // fast-forward time until 1s before it should be executed
+    jest.advanceTimersByTime(199);
+    expect(callback).not.toHaveBeenCalled(); // FAILS
+
+    // jest.getTimerCount() here returns 0
+
+    // fast-forward until 1st call should be executed
+    jest.advanceTimersToNextTimer(1);
+    expect(callback).toHaveBeenCalledTimes(1);
+
+    // fast-forward until next timer should be executed
+    jest.advanceTimersToNextTimer();
+    expect(callback).toHaveBeenCalledTimes(2);
+
+    // fast-forward until 3 more timers should be executed
+    jest.advanceTimersToNextTimer(3);
+    expect(callback).toHaveBeenCalledTimes(5);
+  });
+})
