76422bc23d2275e6881ef886093c9738787fd51a
Vineet Parekh <pvineet44@gmail.com>
2021-04-27 11:48:34 +0530

    backend tests

--- backend/tetromino.test.js
+++
@@ -1,7 +0,0 @@
-// import randomTetrominoArray from './src/tetrominos.ts';
-const { test, expect } = require('@jest/globals');
-
-const {randomTetrominoArray} = require('./src/tetrominos')
-test("Get Tetro test", () => {
-    expect(randomTetrominoArray()).toHaveLength(1000)
-})
